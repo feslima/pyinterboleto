bf70e6c3fdf04f81d6b90b6494e7419a66346352
Felipe <feslima93@gmail.com>
2021-08-14 08:14:33 -0300

    List query tests with the remaining parameters
    Fixes #3 - size parameter

--- tests/query/list.py
+++ tests/query/list.py
@@ -1,11 +1,8 @@
-from datetime import date, timedelta
-from pprint import pprint
-from typing import Dict, List, Tuple
+from datetime import date
 
 import pytest
 from pyinterboleto import Boleto, RequestConfigs
-from pyinterboleto.consulta.lista import BoletoItem, OrdenarEnum
-from tabulate import tabulate
+from pyinterboleto.consulta.lista import FiltrarEnum, OrdenarEnum
 
 
 def ordering_naming(value) -> str:
@@ -36,3 +33,51 @@ def test_ordering(request_configs: RequestConfigs, enum: OrdenarEnum):
         assert default_list == lista
     else:
         assert default_list != lista
+
+
+def filtering_naming(value) -> str:
+    if isinstance(value, FiltrarEnum):
+        return value.value
+
+    return value
+
+
+@pytest.mark.parametrize('enum', list(FiltrarEnum), ids=filtering_naming)
+def test_filtering(request_configs: RequestConfigs, enum: FiltrarEnum):
+    boleto = Boleto(request_configs)
+
+    today: date = date.today()
+
+    year: int = today.year
+
+    inicial = date(year, 1, 1)
+    final = today
+
+    default_list = boleto.consulta_lista(inicial, final)
+    lista = boleto.consulta_lista(inicial, final, filtrar=enum)
+
+    if enum == FiltrarEnum.T:
+        assert default_list == lista
+    else:
+        assert default_list != lista
+
+
+@pytest.mark.parametrize('size', list(range(1, 8)))
+def test_size(request_configs: RequestConfigs, size: int):
+    boleto = Boleto(request_configs)
+
+    today: date = date.today()
+
+    year: int = today.year
+
+    inicial = date(year, 1, 1)
+    final = today
+
+    lista = boleto.consulta_lista(inicial, final, size=size)
+
+    assert lista.numberOfElements == size
+
+
+@pytest.mark.skip(reason="Não há boletos emitidos suficiente para mais de 1 página")
+def test_page_number():
+    assert False
